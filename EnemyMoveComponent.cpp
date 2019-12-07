5e56f82e896dce18b1d164d26ffeaf85745e6d2c
RenzD <renzdionglay@gmail.com>
2019-12-07 00:57:42 -0800

    Continuous movement

--- EnemyMoveComponent.cpp
+++ EnemyMoveComponent.cpp
@@ -2,15 +2,72 @@
 #include "Actor.h"
 
 EnemyMoveComponent::EnemyMoveComponent(class Actor* newOwner, int updateOrder)
-	:Component(newOwner, updateOrder)
+	:Component(newOwner, updateOrder),
+	movingUp(false),
+	movingDown(false),
+	movingLeft(false),
+	movingRight(false),
+	speed(20.0f)
 {
 	game = newOwner->GetGame();
 }
 
 void EnemyMoveComponent::Update(float deltaTime) {
-	if (move && moveable) {
+	if (move && moveable && !movingUp && !movingDown && !movingLeft && !movingRight) {
 		MoveEnemy();
 	}
+
+	if (movingUp) {
+		Vector3 pos = owner->GetPosition();
+		cout << " CURRENT POSITION: " << pos.y << endl;
+
+		pos.y += speed;
+		owner->SetPosition(pos);
+		if (pos.y > dest) { //dest = current pos + 100
+			movingUp = false;
+			pos.y = dest;
+			owner->SetPosition(pos);
+		}
+	}
+	if (movingRight) {
+		Vector3 pos = owner->GetPosition();
+		cout << " CURRENT POSITION: " << pos.x << endl;
+
+		pos.x += speed;
+		owner->SetPosition(pos);
+		if (pos.x > dest) { //dest = current pos + 100
+			movingRight = false;
+			pos.x = dest;
+			owner->SetPosition(pos);
+		}
+	}
+
+
+	if (movingDown) {
+		Vector3 pos = owner->GetPosition();
+		cout << " CURRENT POSITION: " << pos.y << endl;
+
+		pos.y -= speed;
+		owner->SetPosition(pos);
+		if (pos.y < dest) { //dest = current pos + 100
+			movingDown = false;
+			pos.y = dest;
+			owner->SetPosition(pos);
+		}
+	}
+	if (movingLeft) {
+		Vector3 pos = owner->GetPosition();
+		cout << " CURRENT POSITION: " << pos.x << endl;
+
+		pos.x -= speed;
+		owner->SetPosition(pos);
+		if (pos.x < dest) { //dest = current pos + 100
+			movingLeft = false;
+			pos.x = dest;
+			owner->SetPosition(pos);
+		}
+	}
+
 }
 
 void EnemyMoveComponent::MoveEnemy()
@@ -47,29 +104,46 @@ void EnemyMoveComponent::MoveEnemy()
 	tempY = enem_col + y;
 	if (game->IsWalkable(tempX, tempY) == 1)
 	{
-		game->SetWalkable(enem_row, enem_col);
+		if (game->IsWalkable(enem_row, enem_col) != 3) {
 
-		pos2 = owner->GetPosition() + Vector3(x * 100, y * 100, .0f);
-		owner->SetPosition(pos2);
-		game->SetEnemyMapPos(tempX, tempY);
-		
-	}
-	else if (game->IsWalkable(tempX, tempY) == 3)
-	{
-		game->SetWalkable(enem_row, enem_col);
+			if (direction == 0) {
+				movingUp = true;
+				dest = owner->GetPosition().y + 100;
+			}
 
-		pos2 = owner->GetPosition() + Vector3(x * 100, y * 100, .0f);
-		owner->SetPosition(pos2);
-		game->SetEnemyMapPos(tempX, tempY);
-		Collided();
+			if (direction == 1) {
+				movingRight = true;
+				dest = owner->GetPosition().x + 100;
+			}
+			if (direction == 2) {
+				movingDown = true;
+				dest = owner->GetPosition().y - 100;
+			}
+
+			if (direction == 3) {
+				movingLeft = true;
+				dest = owner->GetPosition().x - 100;
+			}
+
+			game->SetWalkable(enem_row, enem_col);
+			//pos2 = owner->GetPosition() + Vector3(x * 100, y * 100, .0f);
+			//owner->SetPosition(pos2);
+			game->SetEnemyMapPos(tempX, tempY);
+		}
 	}
+
+	else if (game->IsWalkable(tempX, tempY) == 3) {
+		/*
+		cout << "Player Encountered " << endl;
+		game->SetWalkable(enem_row, enem_col);
+		//game->SetPlayerMapPos(row, col + -horizontalMove / 100);
+		Vector3 pos = owner->GetPosition() + Vector3(x * 100, y * 100, .0f);
+		owner->SetPosition(pos);
+
+		game->SetEnemyCollision(true);
+		//do something;
+		*/
+	}
+
 	SetMove(false);
-}
-
-
-void EnemyMoveComponent::Collided()
-{
-	cout << "COLLISION: " << game->GetEnemyCollision() << endl;
-	game->SetEnemyCollision(true);
-	cout << "COLLISION: " << game->GetEnemyCollision() << endl;
 }
